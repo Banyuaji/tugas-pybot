9336b25e7801df29e9a2518e851708029d7d876c
Adhi Satria Banyuaji <53932334+Banyuaji@users.noreply.github.com>
2020-06-12 18:26:47 +0700

    tugas bot python

--- Mybot.py
+++ Mybot.py
@@ -1,13 +1,14 @@
 import telebot
-import mysql
+import mysql.connector
 import Mytoken
 
-from datetime import time
 from datetime import datetime
 from telebot import apihelper
 sekarang = datetime.now()
 TOKEN = Mytoken.TOKEN
 mybot = telebot.TeleBot(TOKEN)
+myDB = mysql.connector.connect(host='localhost', user='root', database='db_belajarbot')
+sql = myDB.cursor()
 
 class MyBot:
     def __init__(self):
@@ -15,12 +16,33 @@ class MyBot:
 
     @mybot.message_handler(commands=['start', 'help'])
     def start(message):
-        #photo = open('img/rpl.png, rb')
+        photo = open('img/rpl.png', 'rb')
+        mybot.send_photo(message.from_user.id, photo)
         teks = Mytoken.SAPA + "\n-- Admin & dev Adhi satria banyuaji --" + "\n" \
                                 "-- Hari ini tanggal " + str(sekarang) + " --"
         mybot.reply_to(message, teks)
 
-    @mybot.message_handler()
+    @mybot.message_handler(commands=['datasiswa'])
+    def menu_data_siswa(message):
+        query = "select nipd,nama,kelas from tabel_siswa "
+        sql.execute(query)
+        data = sql.fetchall()
+        jmldata = sql.rowcount
+        kumpuldata = ''
+        if (jmldata > 0):
+            no = 0
+            for x in data:
+                no += 1
+                kumpuldata = kumpuldata + str(x)
+                print(kumpuldata)
+                kumpuldata = kumpuldata.replace('(', '')
+                kumpuldata = kumpuldata.replace(')', '')
+                kumpuldata = kumpuldata.replace("'", '')
+                kumpuldata = kumpuldata.replace(",", '')
+        else:
+            print('data kosong')
+
+        mybot.reply_to(message, str(kumpuldata))
 
 print("Bot sudah berjalan bro...")
 mybot.polling(none_stop=True)
--- Mytoken.py
+++ Mytoken.py
@@ -1,2 +1,4 @@
 TOKEN = '949624848:AAFZxhpoin1GcxnGelZ43H2EBmIer6DHyuw'
-SAPA = "Yoo... whats'up guys, saya adalah Airbot."
+SAPA = "Yoo... whats'up guys, saya adalah Airbot." + "\n" \
+       "Disini saya akan menampilkan beberapa data" + "\n" \
+       "Dari kelas XI RPL"
